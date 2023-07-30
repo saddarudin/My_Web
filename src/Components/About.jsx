2ef15486d4774e980981e16deb3b0b19a653ceff
Saddar U Din Babar <108076528+saddarudin@users.noreply.github.com>
2023-07-30 13:40:17 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,20 +23,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Engineering student studying at Mehran University of Engineering and Technology, Jamshoro. I enjoy  working with data structures and algorithms. I am passionate about solving problems in new creative ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["MySql", "Data Structures", "Java", "Python", "C++", "Maths"];
 
 /**
  * Use this to give more information about what you are passionate about,
